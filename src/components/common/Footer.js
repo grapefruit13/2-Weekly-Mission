935548a14b621df26b1d6c95aa20a2f539091287
grapefruit13 <liam.heo11@gmail.com>
2023-12-07 16:14:01 +0900

    Refactor(mentor): week7- extract FooterSnsDatas component from Footer component

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,32 +1,12 @@
 import "../../assets/styles/footer/Footer.css";
 import Sns from "./Sns.js";
-import facebook from "../../assets/icons/footer/facebook.png";
-import twitter from "../../assets/icons/footer/twitter.png";
-import instagram from "../../assets/icons/footer/instagram.png";
-import youtube from "../../assets/icons/footer/youtube.png";
-
+import FooterSnsDatas from "./FooterSnsDatas.jsx";
 export function Footer() {
   const getThisYear = () => {
     const date = new Date();
     const thisYear = date.getFullYear();
     return thisYear;
   };
-  const footerSnsDatas = [
-    { id: 1, name: "facebook", url: "https://www.facebook.com", img: facebook },
-    { id: 2, name: "twitter", url: "https://www.twitter.com", img: twitter },
-    {
-      id: 3,
-      name: "youtube",
-      url: "https://www.youtube.com",
-      img: youtube,
-    },
-    {
-      id: 4,
-      name: "instagram",
-      url: "https://www.instagram.com",
-      img: instagram,
-    },
-  ];
 
   return (
     <>
@@ -41,7 +21,7 @@ export function Footer() {
           </span>
         </div>
         <div className="footer-sns">
-          {footerSnsDatas.map((data) => {
+          {FooterSnsDatas.map((data) => {
             return (
               <div key={`sns-${data.id}`}>
                 <Sns footerSnsData={data} />
---
+++ src/components/common/FooterSnsDatas.jsx
@@ -0,0 +1,23 @@
+import facebook from "../../assets/icons/footer/facebook.png";
+import twitter from "../../assets/icons/footer/twitter.png";
+import instagram from "../../assets/icons/footer/instagram.png";
+import youtube from "../../assets/icons/footer/youtube.png";
+
+export const FooterSnsDatas = [
+  { id: 1, name: "facebook", url: "https://www.facebook.com", img: facebook },
+  { id: 2, name: "twitter", url: "https://www.twitter.com", img: twitter },
+  {
+    id: 3,
+    name: "youtube",
+    url: "https://www.youtube.com",
+    img: youtube,
+  },
+  {
+    id: 4,
+    name: "instagram",
+    url: "https://www.instagram.com",
+    img: instagram,
+  },
+];
+
+export default FooterSnsDatas;
