bcddca0d4be09d6505a28658176469978272d65d
grapefruit13 <liam.heo11@gmail.com>
2024-01-12 02:32:34 +0900

    feat: filteredLinks(키워드 검색 링크 목록)이 없을 때 메시지 화면에 출력

--- components/common/CardWrapper.tsx
+++ components/common/CardWrapper.tsx
@@ -1,6 +1,7 @@
 import { CardContextProvider } from '../../contexts/CardContext';
 import Card from './Card';
 import styles from '@/styles/card/cardWrapper.module.css';
+import Nolinks from './folderPage/NoLinks';
 
 interface Props {
   links: {
@@ -16,6 +17,7 @@ interface Props {
 export default function CardWrapper({ links }: Props) {
   return (
     <div className={styles.wrapper}>
+      {!links.length && <Nolinks msg="검색한 결과가 없습니다" />}
       {links.map((link) => {
         return (
           <div key={link.id}>
