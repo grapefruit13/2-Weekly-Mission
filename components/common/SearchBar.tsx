b80af839ea49c129f363688b519dad89b73a09ad
grapefruit13 <liam.heo11@gmail.com>
2024-01-12 02:27:26 +0900

    feat: 링크 검색창에 키워드 입력시 쿼리스트링 및 state에 저장되도록 기능 구현

--- components/common/SearchBar.tsx
+++ components/common/SearchBar.tsx
@@ -1,22 +1,56 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import FolderContext from '../../contexts/FolderContext';
 import styles from '@/styles/search/searchbar.module.css';
+import { useRouter } from 'next/router';
 
 export default function SearchBar() {
-  // const { handleSetSearchKeyword } = useContext(FolderContext);
+  const { setKeyword } = useContext(FolderContext);
+  const router = useRouter();
+  const [isSearchActive, setIsSearchActive] = useState(false);
 
-  const handleKeyDown = (e: any) => {
-    if (e.key === 'Enter') {
-      console.log('pressed enter');
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (
+      e.key === 'Enter' ||
+      (e.key === 'Backspace' && router.query.keyword === '')
+    ) {
+      setKeyword(router.query);
+
+      router.push({
+        query: '',
+      });
     }
+  };
+
+  const handleKeywordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.value) {
+      setIsSearchActive(false);
+      router.push({
+        query: { keyword: '' },
+      });
+      return;
+    }
+
+    if (!isSearchActive) {
+      setIsSearchActive(true);
+    }
+
+    router.push({
+      pathname: `${router.pathname}`,
+      query: { keyword: `${e.target.value}` },
+    });
   };
 
   return (
     <>
       <input
-        className={styles.searchbarInput}
+        className={`${styles.searchbarInput} ${
+          isSearchActive ? styles.active : styles.inactive
+        }`}
+        name="keyword"
         type="text"
         placeholder="링크를 검색해 보세요."
+        onChange={(e) => handleKeywordChange(e)}
+        onKeyDown={(e) => handleKeyDown(e)}
       />
     </>
   );
