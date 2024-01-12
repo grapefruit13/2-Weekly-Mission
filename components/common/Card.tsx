9a9f10655e360469ee13546bdb477b005f29c9ff
grapefruit13 <liam.heo11@gmail.com>
2024-01-12 22:28:23 +0900

    refactor(mentor): createdDates state 삭제, import 문 절대경로로 수정

--- components/common/Card.tsx
+++ components/common/Card.tsx
@@ -1,31 +1,35 @@
-import { useContext, useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { calCreatedAt, calCreatedDates } from '../../utils/date';
-import { KebabContextProvider } from '../../contexts/KebabContext';
-import CardContext from '../../contexts/CardContext';
+import { calCreatedAt, calCreatedDates } from '@/utils/date';
+import { KebabContextProvider } from '@/contexts/KebabContext';
+import CardInfo from '@/components/common/CardInfo';
 import NoImg from '@/public/assets/icons/card/card_no-img.svg';
 import kebab from '@/public/assets/icons/card/kebab.svg';
-import CardInfo from './CardInfo';
 import styles from '@/styles/card/card.module.css';
-interface Props {
-  id: string;
-  createdAt: string;
-  url: string;
-  title: string;
-  description: string;
-  imageSource: string;
+
+interface Link {
+  link: {
+    id: string;
+    createdAt: string;
+    url: string;
+    title: string;
+    description: string;
+    imageSource: string;
+  };
 }
 
-export default function Card({ link }: { link: Props }) {
-  const { setLinkInfo } = useContext(CardContext);
+export default function Card({ link }: Link) {
   const { id, createdAt, url, title, description, imageSource } = link;
-  const [mins, setMins] = useState('');
-  const [createdDates, setCreatedDates] = useState({
-    year: '',
-    month: '',
-    day: '',
+  const [linkInfo, setLinkInfo] = useState({
+    id: '',
+    createdAt: '',
+    url: '',
+    title: '',
+    description: '',
+    imageSource: '',
   });
+  const [mins, setMins] = useState('');
   const [isHovered, setIsHovered] = useState(false);
 
   const handleSetLinkInfo = () => {
@@ -39,76 +43,59 @@ export default function Card({ link }: { link: Props }) {
     });
   };
 
-  const getCreatedDates = () => {
+  const getCreatedAt = useMemo(() => {
     const [year, month, day] = calCreatedDates(createdAt);
-    setCreatedDates((prev) => ({
-      ...prev,
-      year: year,
-      month: month,
-      day: day,
-    }));
-  };
-
-  const getCreatedAt = () => {
-    setMins(calCreatedAt(createdDates));
-  };
+    setMins(calCreatedAt(year, month, day));
+    return { year, month, day };
+  }, [createdAt]);
 
   useEffect(() => {
     handleSetLinkInfo();
   }, []);
 
-  useEffect(() => {
-    getCreatedDates();
-  }, [createdAt]);
-
-  useEffect(() => {
-    getCreatedAt();
-  }, [createdDates]);
-
   return (
-    <>
-      <KebabContextProvider>
-        <div className={styles.flexWrapper} id={`card-${id}`}>
-          <div className={styles.cardImgWrapper}>
-            <Link target="_blank" href={url}>
-              {imageSource ? (
-                <Image
-                  className={
-                    isHovered
-                      ? `${styles.cardImg} ${styles.grow}`
-                      : styles.cardImg
-                  }
-                  width={500}
-                  height={253}
-                  src={imageSource}
-                  alt={`${title}-img`}
-                  onMouseEnter={() => {
-                    setIsHovered(true);
-                  }}
-                  onMouseLeave={() => {
-                    setIsHovered(false);
-                  }}
-                />
-              ) : (
-                <Image
-                  className={styles.cardImg}
-                  width={500}
-                  height={253}
-                  src={NoImg}
-                  alt={`${title}-img`}
-                />
-              )}
-            </Link>
-          </div>
-          <CardInfo
-            mins={mins}
-            imgSrc={kebab}
-            title={title}
-            description={description}
-            createdDates={createdDates}
-          />
+    <KebabContextProvider>
+      <div className={styles.flexWrapper} id={`card-${id}`}>
+        <div className={styles.cardImgWrapper}>
+          <Link target="_blank" href={url}>
+            {imageSource ? (
+              <Image
+                className={
+                  isHovered
+                    ? `${styles.cardImg} ${styles.grow}`
+                    : styles.cardImg
+                }
+                width={500}
+                height={253}
+                src={imageSource}
+                alt={`${title}-img`}
+                onMouseEnter={() => {
+                  setIsHovered(true);
+                }}
+                onMouseLeave={() => {
+                  setIsHovered(false);
+                }}
+              />
+            ) : (
+              <Image
+                className={styles.cardImg}
+                width={500}
+                height={253}
+                src={NoImg}
+                alt={`${title}-img`}
+              />
+            )}
+          </Link>
         </div>
-      </KebabContextProvider>
-    </>
+        <CardInfo
+          mins={mins}
+          imgSrc={kebab}
+          title={title}
+          description={description}
+          createdDates={getCreatedAt}
+          linkInfo={linkInfo}
+        />
+      </div>
+    </KebabContextProvider>
   );
 }
