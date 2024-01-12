9b2839d5b19e7c08ad658f5197b2d5fe55ca8a4e
grapefruit13 <liam.heo11@gmail.com>
2024-01-12 22:35:53 +0900

    refactor(mentor) : interface inline 적용, return 문에 옵셔널체이닝 사용하여 에러 방지

--- components/common/CardWrapper.tsx
+++ components/common/CardWrapper.tsx
@@ -1,9 +1,10 @@
-import { CardContextProvider } from '../../contexts/CardContext';
-import Card from './Card';
+import Card from '@/components/common/Card';
 import styles from '@/styles/card/cardWrapper.module.css';
-import Nolinks from './folderPage/NoLinks';
+import Nolinks from '@/components/common/folderPage/NoLinks';
 
-interface Props {
+export default function CardWrapper({
+  links,
+}: {
   links: {
     id: string;
     createdAt: string;
@@ -12,18 +13,14 @@ interface Props {
     imageSource: string;
     description: string;
   }[];
-}
-
-export default function CardWrapper({ links }: Props) {
+}) {
   return (
     <div className={styles.wrapper}>
-      {!links.length && <Nolinks msg="검색한 결과가 없습니다" />}
-      {links.map((link) => {
+      {!links?.length && <Nolinks msg="검색한 결과가 없습니다" />}
+      {links?.map((link) => {
         return (
           <div key={link.id}>
-            <CardContextProvider>
-              <Card link={link} />
-            </CardContextProvider>
+            <Card link={link} />
           </div>
         );
       })}
