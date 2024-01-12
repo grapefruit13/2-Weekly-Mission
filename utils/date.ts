e65ea62aa547a636a21786df6e7b5e66b7a2b466
grapefruit13 <liam.heo11@gmail.com>
2024-01-12 22:29:55 +0900

    refactor(mentor) : calCreatedAt 파라미터를 객체에서 string 변수 3개로 변경

--- utils/date.ts
+++ utils/date.ts
@@ -1,21 +1,17 @@
 export function calCreatedDates(createdAt: string) {
-  const splited: string[] = createdAt.split("T").slice(0, 1);
-  const yearMonthDay: string[] = splited[0].split("-");
+  const splited: string[] = createdAt.split('T').slice(0, 1);
+  const yearMonthDay: string[] = splited[0].split('-');
 
   return yearMonthDay;
 }
 
-export function calCreatedAt(createdDates: {
-  year: string;
-  month: string;
-  day: string;
-}) {
+export function calCreatedAt(year: string, month: string, day: string) {
   const now = new Date();
 
   const createdDate = new Date(
-    Number(createdDates.year),
-    Number(createdDates.month) * 1 - 1,
-    Number(createdDates.day)
+    Number(year),
+    Number(month) * 1 - 1,
+    Number(day),
   );
 
   const diffMSec = now.getTime() - createdDate.getTime();
@@ -34,21 +30,21 @@ export function calCreatedAt(createdDates: {
   if (months > 23) {
     return `${Math.floor(months / 12)} years ago`;
   } else if (months >= 12) {
-    return "1 year ago";
+    return '1 year ago';
   } else if (days > 30) {
     return `${months} month ago`;
   } else if (days <= 30) {
     return `${days} days ago`;
   } else if (hours >= 24) {
-    return "1 day ago";
+    return '1 day ago';
   } else if (hours <= 23) {
     return `${hours} hours ago`;
   } else if (minutes >= 60) {
-    return "1 hour ago";
+    return '1 hour ago';
   } else if (minutes <= 59) {
     return `${minutes} minutes ago`;
   } else if (minutes < 2) {
-    return "1 minute ago";
+    return '1 minute ago';
   }
-  return "";
+  return '';
 }
