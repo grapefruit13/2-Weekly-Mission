95ff62918d865c274fe99decdf737585adc15a96
grapefruit13 <liam.heo11@gmail.com>
2024-01-15 01:19:14 +0900

    feat: 회원가입/로그인 시 아이디/비밀번호 유효성 검사 기능 구현

---
+++ utils/validation.ts
@@ -0,0 +1,9 @@
+export function validateEmail(email: string) {
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  return emailRegex.test(email);
+}
+
+export function validatePassword(password: string) {
+  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+  return passwordRegex.test(password);
+}
