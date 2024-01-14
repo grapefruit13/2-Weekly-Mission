18847c135d1dcfcf8533288c0200c732446e741d
grapefruit13 <liam.heo11@gmail.com>
2024-01-15 03:33:15 +0900

    refactor(styles): 필요없는 클래스 선택자 삭제

--- components/common/SearchResult.tsx
+++ components/common/SearchResult.tsx
@@ -1,12 +1,16 @@
 import styles from '@/styles/search/searchResult.module.css';
+import { useRouter } from 'next/router';
 
 export default function SearchResult() {
+  const router = useRouter();
+  const search = router.query.keyword;
+
   return (
-    <div className={styles.searchResultContainer}>
+    <>
       <span className={styles.resultMessage}>
-        <span className={styles.keyword}>코드잇</span>
+        <span className={styles.keyword}>{search}</span>
         <span className={styles.sub}>으로 검색한 결과입니다.</span>
       </span>
-    </div>
+    </>
   );
 }
