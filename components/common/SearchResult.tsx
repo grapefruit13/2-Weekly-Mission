072b67d556a43ad6378d4bb68621a0d13a94dc98
grapefruit13 <liam.heo11@gmail.com>
2024-01-17 21:44:12 +0900

    refactor(mentor): 검색키워드 보여주는 컴포넌트에서 keyword context에서 불러오도록 수정

--- components/common/SearchResult.tsx
+++ components/common/SearchResult.tsx
@@ -1,16 +1,14 @@
-import { useRouter } from 'next/router';
+import { useContext } from 'react';
+import FolderContext from '@/contexts/FolderContext';
 import styles from '@/styles/search/searchResult.module.css';
 
 export default function SearchResult() {
-  const router = useRouter();
-  const search = router.query.keyword;
+  const { keyword } = useContext(FolderContext);
 
   return (
-    <>
-      <span className={styles.resultMessage}>
-        <span className={styles.keyword}>{search}</span>
-        <span className={styles.sub}>으로 검색한 결과입니다.</span>
-      </span>
-    </>
+    <span className={styles.resultMessage}>
+      <span className={styles.keyword}>{keyword}</span>
+      <span className={styles.sub}>으로 검색한 결과입니다.</span>
+    </span>
   );
 }
