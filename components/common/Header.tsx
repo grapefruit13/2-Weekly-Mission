3840261ec1c2e61ea770f8fb803acc12340987ed
grapefruit13 <liam.heo11@gmail.com>
2024-01-22 00:29:47 +0900

    refactor: props에 타입설정, props 기본 값 삭제

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -3,23 +3,17 @@ import LoginButton from '@/components/common/LoginButton';
 import Logo from '@/components/common/Logo';
 import styles from '@/styles/header/header.module.css';
 
-interface Props {
-  profileData: {
-    id: number;
-    created_at?: string;
-    name: string;
-    image_source: string;
-    email: string;
-    auth_id?: string;
-  };
+interface HeaderProps {
+  id: number;
+  created_at?: string;
+  name: string;
+  image_source: string;
+  email: string;
+  auth_id?: string;
 }
 
-export default function Header({ profileData }: Props) {
-  const {
-    name = 'defaultName',
-    image_source = 'https://images.unsplash.com/photo-1701600713610-0f724c65168d?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    email = 'default@email.com',
-  } = profileData;
+export default function Header({ profileData }: { profileData: HeaderProps }) {
+  const { id, created_at, name, image_source, email, auth_id } = profileData;
 
   return (
     <div className={styles.header}>
