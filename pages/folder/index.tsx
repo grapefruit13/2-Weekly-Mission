fbba917298ef37218315001f169a28e2adc0eb10
grapefruit13 <liam.heo11@gmail.com>
2024-01-22 00:25:43 +0900

    feat: folder 페이지에서 14주차 요구사항 구현([id] 페이지 라우터 제외)

--- pages/folder/index.tsx
+++ pages/folder/index.tsx
@@ -1,5 +1,11 @@
 import { useContext, useEffect, useState } from 'react';
-import { getData } from '@/utils/api';
+import {
+  getAllFolders,
+  getFolders,
+  getLinks,
+  getLinksByFolder,
+  getUsers,
+} from '@/utils/api';
 import { filterByKeyword } from '@/utils/searchUtils';
 import FolderContext from '@/contexts/FolderContext';
 import Modal from '@/components/common/folderPage/modal/Modal';
@@ -12,8 +18,19 @@ import Nolinks from '@/components/common/folderPage/NoLinks';
 import CardWrapper from '@/components/common/CardWrapper';
 import FolderAddButton from '@/components/common/folderPage/FolderAddButton';
 import styles from '@/styles/card/cardWrapper.module.css';
+import { getToken, setToken } from '@/utils/auth';
+import { useRouter } from 'next/router';
 
 export default function Folder() {
+  const router = useRouter();
+  const [profileData, setProfileData] = useState({
+    id: 0,
+    created_at: '',
+    name: '',
+    image_source: '',
+    email: '',
+    auth_id: '',
+  });
   const {
     addedLink,
     clickedOption,
@@ -23,34 +40,24 @@ export default function Folder() {
     filteredLinks,
     setFilteredLinks,
   } = useContext(FolderContext);
-  const [profileData, setProfileData] = useState({
-    id: 0,
-    created_at: '',
-    name: '',
-    image_source: '',
-    email: '',
-    auth_id: '',
-  });
   const [links, setLinks] = useState([]);
   const [currentFolder, setCurrentFolder] = useState<{
-    id: string;
+    id: number;
     name?: string;
-  }>({ id: '', name: '전체' });
+  }>({ id: 0, name: '전체' });
 
   const getUserData = async () => {
     try {
-      const result = await getData('users/1');
-      const { id, created_at, name, image_source, email, auth_id } =
-        result.data[0];
-      setProfileData((prevProfileData) => ({
-        ...prevProfileData,
+      const result = await getUsers();
+      const { id, created_at, name, image_source, email, auth_id } = result;
+      setProfileData({
         id,
         created_at,
         name,
         image_source,
         email,
         auth_id,
-      }));
+      });
     } catch (e) {
       throw new Error(`Folderpage getUserData ${e}`);
     }
@@ -58,9 +65,9 @@ export default function Folder() {
 
   const getTotalLinksData = async () => {
     try {
-      const result = await getData('users/1/links');
-      const { data } = result;
-      const links = data.map((link: any) => ({
+      const result = await getLinks(profileData.id);
+      console.log(result, 'dd');
+      const links = result.map((link: any) => ({
         ...link,
         createdAt: link.created_at,
         imageSource: link.image_source,
@@ -73,10 +80,9 @@ export default function Folder() {
 
   const getFolderList = async () => {
     try {
-      const result = await getData('users/1/folders');
-      const { data } = result;
-      data.unshift({ name: '전체' });
-      setFolderList(data);
+      const res = await getAllFolders(profileData.id);
+      res.unshift({ name: '전체' });
+      setFolderList(res);
     } catch (e) {
       throw new Error(`Folderpage의 getFolderList에서 ${e} 발생`);
     }
@@ -84,16 +90,13 @@ export default function Folder() {
 
   const getFolder = async () => {
     try {
-      const result = await getData(
-        `users/1/links?folderId=${currentFolder?.id}`,
-      );
-      const { data } = result;
-      const datas = data.map((link: any) => ({
+      const res = await getLinksByFolder(profileData.id, currentFolder.id);
+      const data = res.map((link: any) => ({
         ...link,
         createdAt: link.created_at,
         imageSource: link.image_source,
       }));
-      setLinks(datas);
+      setLinks(data);
     } catch (e) {
       throw new Error(
         `Folderpage의 handleFolderClick의 getFolder에서 ${e} 발생`,
@@ -101,26 +104,32 @@ export default function Folder() {
     }
   };
 
-  const handleFolderClick = (
-    e: React.MouseEvent<HTMLDivElement, MouseEvent>,
-  ) => {
-    const textContent = e.currentTarget.textContent;
-    setCurrentFolder((prev) => ({ ...prev, name: textContent?.toString() }));
-    if (textContent === '전체') {
+  const handleFolderClick = (folderId: number) => {
+    if (!folderId) {
+      setCurrentFolder((prev) => ({ ...prev, name: '전체' }));
       getTotalLinksData();
       return;
     }
     const clikedFolder: any = folderList.filter(
-      (folder: any) => folder.name === textContent,
+      (folder: any) => folder.id === folderId,
     );
     setCurrentFolder(clikedFolder[0]);
+    if (folderId) {
+      router.push(`/folder/${folderId}`);
+    }
   };
 
   useEffect(() => {
+    if (!getToken()) {
+      router.replace('/signin');
+    }
     getUserData();
+  }, []);
+
+  useEffect(() => {
     getTotalLinksData();
     getFolderList();
-  }, []);
+  }, [profileData.id]);
 
   useEffect(() => {
     getFolder();
@@ -134,18 +143,18 @@ export default function Folder() {
     <>
       {clickedOption.addFolderLink && addedLink && (
         <Modal
-          title="폴더에 추가"
+          title='폴더에 추가'
           button={{ color: 'blue', text: '추가하기' }}
           subtitle={addedLink}
           folderList={folderList}
         />
       )}
       {clickedOption.shareFolder && (
-        <Modal title="폴더 공유" subtitle={currentFolder?.name} share />
+        <Modal title='폴더 공유' subtitle={currentFolder?.name} share />
       )}
       {clickedOption.editFolderName && (
         <Modal
-          title="폴더 이름 변경"
+          title='폴더 이름 변경'
           folderName={currentFolder?.name}
           input
           button={{ color: 'blue', text: '변경하기' }}
@@ -153,14 +162,14 @@ export default function Folder() {
       )}
       {clickedOption.deleteFolder && (
         <Modal
-          title="폴더 삭제"
+          title='폴더 삭제'
           subtitle={currentFolder?.name}
           button={{ color: 'red', text: '삭제하기' }}
         />
       )}
       {clickedOption.addNewFolder && (
         <Modal
-          title="폴더 추가"
+          title='폴더 추가'
           input
           button={{ color: 'blue', text: '추가하기' }}
         />
@@ -179,7 +188,7 @@ export default function Folder() {
                     return (
                       <div
                         key={`folder-${folder.id}`}
-                        onClick={(e) => handleFolderClick(e)}
+                        onClick={() => handleFolderClick(folder.id)}
                       >
                         <Folders
                           className={`${styles.listedFolderName} ${
@@ -193,7 +202,7 @@ export default function Folder() {
                     );
                   })
                 ) : (
-                  <Nolinks msg="folderList가 없습니다." />
+                  <Nolinks msg='folderList가 없습니다.' />
                 )}
               </div>
               <FolderAddButton />
@@ -213,7 +222,7 @@ export default function Folder() {
             )}
           </div>
         )}
-        {!folderList.length && <Nolinks msg="저장된 링크가 없습니다" />}
+        {!folderList.length && <Nolinks msg='저장된 링크가 없습니다' />}
       </div>
     </>
   );
